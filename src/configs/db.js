01d622379b284594603d90baba1a76bf20fd4738
MohitMaurya <mohitmaurya116@gmail.com>
2021-11-20 10:14:45 +0530

    changes to the product model

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const connect = () => {
-    return mongoose.connect("mongodb://localhost:27017/hackathon");
+    return mongoose.connect(process.env.MONGODB_URI);
 }
 
 module.exports = connect;
--- src/models/product.model.js
+++ src/models/product.model.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-    name: { type: String, required: true },
+    id: { type: Number, required: true },
+    description: { type: String, required: true },
+    currency: { type: String, required: true },
     price: { type: Number, required: true },
-    image: { type: String, required: true },
+    thumb: { type: String, required: true },
 }, {
     versionKey: false,
     timestamps: true
